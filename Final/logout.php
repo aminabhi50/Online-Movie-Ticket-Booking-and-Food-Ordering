886776d11f05c69e33afe41ab1ef8f5519462334
aminabhi50 <41742726+aminabhi50@users.noreply.github.com>
2022-02-24 13:39:04 -0500

    Delete Final directory

--- Final/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-echo "<script type='text/javascript'>";
-session_destroy();
-echo "alert('You have successfully logged out.');";
-echo "window.location='login.html';";
-echo "</script>";
-exit;
-?>
